a3a1ec7682b805901133d24b67f2acdf47ad15fa
MorelK1 <kouhossounon.vianney@gmail.com>
2022-09-05 13:03:12 +0100

    ajout de la barre de navigation

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, session
+# import os.path
+# from keras.models import load_model
+# from keras.preprocessing import image
+# from PIL import Image
+# import numpy as np
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!fghj'
+def detection():  # put application's code here
+    return render_template('detection.html')
 
 
 if __name__ == '__main__':
