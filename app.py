b04a128ebfa0244b594fc59efe5b1326e5d79303
MorelK1 <kouhossounon.vianney@gmail.com>
2022-09-06 12:41:39 +0100

    ajout du la fonction d'enregistrement des images

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 import os.path
-import numpy
-import tensorflow
 from keras.models import load_model
+from keras.preprocessing import image
 from PIL import Image
-import cv2
+import numpy as np
 
 app = Flask(__name__)
 
 
-@app.route('/')
+def upload():
+    img = request.files["img"]
+    r, e = os.path.splitext(img.filename)
+    img_path = "static/images/predict" + e
+    img.save(img_path)
+    return "static\\images\\predict" + e
+
+
+@app.route('/', methods=['POST', 'GET'])
 def detection():  # put application's code here
-    return render_template('detection.html')
+    if request.method == 'POST':
+        path = upload()
+        return "<p>"+path+"</p>"
+    else:
+        return render_template('detection.html')
 
 
 if __name__ == '__main__':
